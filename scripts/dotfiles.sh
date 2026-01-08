aeb8b663975f09487d1b8513f74d9c8cc56712a7
jusoaresg <julianosgreg@gmail.com>
2026-01-07 22:18:59 -0300

    feat: add nvim config clone

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -4,3 +4,14 @@ ROOT="${1:?No root}"
 RSYNC_FLAGS="${2:-}"
 
 rsync $RSYNC_FLAGS "${ROOT}/configs/" "${HOME}/.config/"
+
+NVIM_CONFIG_DIR="${HOME}/.config/nvim"
+NVIM_REPO="https://github.com/jusoaresg/nvim-conf"
+
+if [[ -d "$NVIM_CONFIG_DIR" ]]; then
+    echo "Updating existing Neovim config..."
+    git -C "$NVIM_CONFIG_DIR" pull
+else
+    echo "Cloning Neovim config..."
+    git clone "$NVIM_REPO" "$NVIM_CONFIG_DIR"
+fi
