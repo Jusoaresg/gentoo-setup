c27fe70bbb0a8c9c06516cd0164cf1abdc28ddc5
jusoaresg <julianosgreg@gmail.com>
2026-02-07 13:46:15 -0300

    feat(nvim): better nvim clone and remote using
    - Now when cloning or updating neovim will have two remotes origin for
      ssh and origin-http(default for updates)

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -11,12 +11,35 @@ rsync $RSYNC_FLAGS "${ROOT}/dotfiles/fonts" "${HOME}/.fonts"
 rsync $RSYNC_FLAGS "${ROOT}/dotfiles/bin/" "${HOME}/.bin/"
 
 NVIM_CONFIG_DIR="${HOME}/.config/nvim"
-NVIM_REPO="https://github.com/jusoaresg/nvim-conf"
+NVIM_REPO_HTTP="https://github.com/jusoaresg/nvim-conf"
+NVIM_REPO_SSH="git@github.com:jusoaresg/nvim-conf"
 
-if [[ -d "$NVIM_CONFIG_DIR" ]]; then
-    echo "Updating existing Neovim config..."
-    git -C "$NVIM_CONFIG_DIR" pull
+if [[ -d "$NVIM_CONFIG_DIR/.git" ]]; then
+
+    echo "Updating neovim config..."
+    (
+        cd "$NVIM_CONFIG_DIR" || exit 1
+
+        if git remote get-url origin-http >/dev/null 2>&1; then
+            git pull origin-http main
+        else
+            git pull
+            git remote rename origin origin-http
+            git remote add origin "$NVIM_REPO_SSH"
+        fi
+    )
 else
-    echo "Cloning Neovim config..."
-    git clone "$NVIM_REPO" "$NVIM_CONFIG_DIR"
+
+    echo "Cloning neovim config..."
+    git clone "$NVIM_REPO_HTTP" "$NVIM_CONFIG_DIR"
+
+    (
+        cd "$NVIM_CONFIG_DIR" || exit 1
+        git remote rename origin origin-http
+        git remote add origin "$NVIM_REPO_SSH"
+
+        git fetch origin
+        git branch --set-upstream-to=origin/$(git symbolic-ref --short HEAD)
+    )
 fi
+
